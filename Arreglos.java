7a5554e1130f6ef9064a2d6b52e56f71e39893ba
palafoxRen <ren.palagarcia@gmail.com>
2025-03-27 13:05:02 -0600

    Calculando promedios individuales

--- Arreglos.java
+++ Arreglos.java
@@ -5,24 +5,44 @@ public class Arreglos {
 
     public static int t = 25; //tamaño máximo del grupo (variable global)
 
-    public static double[] leerCalificaciones(double[] calificaciones){
+    public static double[] leerCalificaciones(){
+        double[] calificaciones = new double[t];
         Scanner sc = new Scanner(System.in);
-        for (int i=0; i<calificaciones.length; i++){
-            System.out.println("Escribe la calificación [" + i + "]: ");
-            calificaciones[i] = sc.nextDouble();
-        }
+            for (int i=0; i<calificaciones.length; i++){
+                System.out.println("Escribe la calificación [" + i + "]: ");
+                calificaciones[i] = sc.nextDouble();
+            }
         return calificaciones;
     }
 
     public static void imprimirCalificaciones(double[] calificaciones){
-
         System.out.println("Contendido del arreglo calificaciones: ");
         for ( double calificacion : calificaciones )
             System.out.println(calificacion);
     }
 
-    public static void evaluarCalificaciones(){
+    public static double[] obtenerPromInd(double[] p1, double[] p2, double[] p3, double[] p4){
+        double[] resultado = new double[t];
+        for (int i=0; i<resultado.length; i++)
+            resultado[i] = (p1[i] + p2[i] + p3[i] + p4[i]) / 4;
+        return resultado;
         
+
+    }
+    public static boolean [] evaluarCalificaciones(double[] proms){
+        boolean [] resultado = new boolean [t];
+        for ( int i=0; i<proms.length; i++ ){
+            if ( proms[i] >=7 )
+                resultado[i] = true;
+            else 
+                resultado[i] = false;
+        }
+        return resultado;
+    }
+
+    public static void imprimirResultados(boolean[] resultados){
+        for ( boolean resultado : resultados )
+            System.out.println(resultado);
     }
 
     public static void main(String[] args) {
@@ -31,31 +51,36 @@ public class Arreglos {
         boolean[] aprobados; 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Programa que calcula el promedio final de un grupo");
-        System.out.println("Escriba el tamaño del grupo: ");
-        t = sc.nextInt();
-
-        //construir los arreglos
-        parcial1 = new double[t];
-        parcial2 = new double[t];
-        parcial3 = new double[t];
-        parcial4 = new double[t];
-        promInd = new double[t];
-        aprobados = new boolean[t];
-
-        //leer las calificaciones de los parciales de cada alumno
+            System.out.println("Programa que calcula el promedio final de un grupo");
+            System.out.println("Escriba el tamaño del grupo: ");
+            t = sc.nextInt();
+            
+            //construir los arreglos
+            parcial1 = new double[t];
+            parcial2 = new double[t];
+            parcial3 = new double[t];
+            parcial4 = new double[t];
+            promInd = new double[t];
+            aprobados = new boolean[t];
+            
+            //leer las calificaciones de los parciales de cada alumno
             System.out.println("-----------------------------------");
             System.out.println("Lectura de calificaciones del parcial 1");
-            parcial1 = leerCalificaciones(parcial1);
+            parcial1 = leerCalificaciones();
             System.out.println("-----------------------------------");
             System.out.println("Lectura de calificaciones del parcial 2");
-            parcial2 = leerCalificaciones(parcial2);
+            parcial2 = leerCalificaciones();
             System.out.println("-----------------------------------");
             System.out.println("Lectura de calificaciones del parcial 3");
-            parcial3 = leerCalificaciones(parcial3);
+            parcial3 = leerCalificaciones();
             System.out.println("-----------------------------------");
             System.out.println("Lectura de calificaciones del parcial 5");
-            parcial4 = leerCalificaciones(parcial4);
+            parcial4 = leerCalificaciones();
+
+            //calcular promedio individual
+            promInd = obtenerPromInd(parcial1, parcial2, parcial3, parcial4);
+            //evaluar si cada estudiante está aprobado o no
+            aprobados = evaluarCalificaciones(promInd);
 
             //mostrar calificaciones de los parciales
             System.out.println("-----------------------------------");
@@ -70,8 +95,13 @@ public class Arreglos {
             System.out.println("-----------------------------------");
             System.out.println("Calificaciones del parcial 4");
             imprimirCalificaciones(parcial4);
+            System.out.println("-----------------------------------");
+            System.out.println("Promedios individuales");
+            imprimirCalificaciones(promInd);
+            System.out.println("-----------------------------------");
+            System.out.println("Estudiantes aprobados");
+            imprimirResultados(aprobados);
 
-
-        sc.close();    
+            sc.close();   
     }
 }
